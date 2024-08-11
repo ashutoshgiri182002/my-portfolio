c4c6af23aab0a5ebde2b97f39e00661a5729b30e
Ashutosh <ashutoshgiri182002@gmail.com>
2024-08-11 18:03:51 +0530

    11-08-2024 Created Hero.jsx and Navbar component for Mobile as well as Desktop view

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='relative h-full overflow-y-auto antialiased'>
+      <div className='fixed inset-0 bg-fixed bg-cover bg-center bg-img'></div>
+      <div className='relative z-10 flex flex-col items-center p-4 space-y-8 container mx-auto'>
+        <Hero />
+        <Navbar />
+      </div>
     </div>
   )
 }
