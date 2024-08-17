892a1fde2ff60762a46425a6a01a8a00d2ac4a4b
Ashutosh <ashutoshgiri182002@gmail.com>
2024-08-17 15:18:47 +0530

    17-08-2024 Solved the Issue with the scrolling in the Skills navigation link now it correctly working

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,5 @@
 import { SKILLS } from "../constants"
-import {motion, stagger} from "framer-motion"
+import { motion } from "framer-motion"
 
 const containerVariants = {
     hidden: {
@@ -34,21 +34,21 @@ const itemVariants = {
 
 const Skills = () => {
     return (
-        <div className="container mx-auto" id="skils">
+        <div className="container mx-auto" id="skills">
             <h2 className="mb-12 mt-20 text-center text-4xl font-semibold">Skills</h2>
             <motion.div
-            initial="hidden"
-            whileInView="visible"
-            variants={containerVariants}
-            viewport={{once: true}}
-             className="mx-2 flex flex-col rounded-3xl px-4 py-2 lg:px-20 border border-stone-50/30">
-                {SKILLS.map((skill,index) => (
+                initial="hidden"
+                whileInView="visible"
+                variants={containerVariants}
+                viewport={{ once: true }}
+                className="mx-2 flex flex-col rounded-3xl px-4 py-2 lg:px-20 border border-stone-50/30">
+                {SKILLS.map((skill, index) => (
                     <motion.div
-                    variants={itemVariants}
-                     key={index} className={`py-6 flex items-center justify-between ${index !== SKILLS.length-1 ? 'border-b border-stone-50/30' : ''}`}>
-                        <div className=" flex items-center"> 
-                        {skill.icon}
-                        <h3 className="px-6 text-lg lg:text-2xl">{skill.name}</h3>
+                        variants={itemVariants}
+                        key={index} className={`py-6 flex items-center justify-between ${index !== SKILLS.length - 1 ? 'border-b border-stone-50/30' : ''}`}>
+                        <div className=" flex items-center">
+                            {skill.icon}
+                            <h3 className="px-6 text-lg lg:text-2xl">{skill.name}</h3>
                         </div>
                         <div className="text-md font-semibold lg:text-xl">
                             <span>{skill.experience}</span>
